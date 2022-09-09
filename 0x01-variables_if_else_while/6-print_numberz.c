6e18e7d9d60381b22349131f6a02206aa2c71a5b
ernest028 <earnestkinyua28@gmail.com>
2022-09-09 05:11:21 -0700

    Task six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+/**
+ *  * main - prints all single base 10 numbers
+ *
+ *   * Return: 0
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+		int number;
+
+
+
+		for (number = 0; number < 10; number++)
+
+		putchar(number + '0');
+
+
+		putchar('\n');
+
+
+
+		return (0);
+}
