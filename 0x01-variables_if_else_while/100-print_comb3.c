8c955e2c54dd605c23d199543eb44b05e474b660
ernest028 <earnestkinyua28@gmail.com>
2022-09-09 06:30:38 -0700

    task ten

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+/**
+ *  * main - prints all possible combinations of 2 digits
+ *
+ *   * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+
+		int i, j;
+
+
+
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = 0; j <= 9; j++)
+				if (i == j || i > j)
+																					continue;
+				putchar(i + '0');
+				putchar(j + '0');
+				if ((i + j) != 17)
+				{
+				putchar(',');
+																				putchar(' ');
+																				}
+			}
+		}
+		putchar('\n');
+		return (0);
+
+}
