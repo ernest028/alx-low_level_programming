76e61d30cf2884c1a1927c89e4abd763e6c2886b
ernest028 <earnestkinyua28@gmail.com>
2022-09-09 05:35:08 -0700

    task eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+
+/**
+ *  * main - prints all numbers of base 16
+ *
+ *   * Return: 0
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+		int number, result;
+
+
+		for (number = 0; number < 16; number++)
+		{
+
+		if (number == 10)
+		result = 'A' - 16;/*result = 65 - 16*/
+		else if (number == 11)
+		result = 'B' - 16;
+		else if (number == 12)
+		result = 'C' - 16;
+		else if (number == 13)
+		result = 'D' - 16;
+		else if (number == 14)
+		result = 'E' - 16;
+
+																		else if (number == 15)
+
+		result = 'F' - 16;
+
+		else
+																		result = number;
+
+																		putchar(result + '0');/*'0' has ASCII value of 48, hence output = result + 48*/
+
+																		}
+		putchar('\n');
+		return (0);
+
+}
