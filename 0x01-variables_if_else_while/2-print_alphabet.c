ddf2b99599331bd3ee15a9bf5b338d02cf9e21a1
ernest028 <earnestkinyua28@gmail.com>
2022-09-09 03:52:05 -0700

    task two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints all loer case letters
+ *
+ *   * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
