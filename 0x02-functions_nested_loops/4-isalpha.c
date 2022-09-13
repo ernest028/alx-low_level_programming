752c8ac55cf9f81eb6a6d5f797978f3b7bd92ddc
ernest028 <earnestkinyua28@gmail.com>
2022-09-13 16:07:21 -0700

    task four

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,11 @@
 
 
 /**
- *  * _islower - checks for lowercase character
+ * _isalpha - checks for alphabet character
  *
- *   * @c: character to check
+ *  * @c: character to check
  *
- *    * Return: 1 if lowercase, 0 if uppercase
- *
+ *   * Return: 1 if lower or uppercase, 0 if not alphabet character*
  */
 
 
