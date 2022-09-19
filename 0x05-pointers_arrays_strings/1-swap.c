f255bce916b604a57b19f0a76217c3086022fb96
ernest028 <earnestkinyua28@gmail.com>
2022-09-19 12:28:53 -0700

    task one

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *  * swap_int - swaps pointers of arguements
+ *
+ *   * @a: whose pointer is swapped with *b
+ *
+ *    * @b: whose pointer is swapped with *a
+ */
+
+
+
+void swap_int(int *a, int *b)
+
+{
+
+	int c;
+
+
+
+	c = *a;
+
+	*a = *b;
+
+	*b = c;
+
+}
